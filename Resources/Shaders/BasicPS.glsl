b52584b8c846b9c4782339df085786460579472f
Yang Kyowon <kyowonhorang@gmail.com>
2018-10-22 14:23:30 +0900

    Draw Wooden Container

--- Resources/Shaders/BasicPS.glsl
+++ Resources/Shaders/BasicPS.glsl
@@ -2,10 +2,14 @@
 out vec4 FragColor;
 uniform vec3 ourColor;
 in vec3 colorOut;
+in vec2 texCoordOut;
+
+uniform sampler2D ourTexture;
 
 void main()
 {
-	FragColor = vec4(clamp(colorOut.x + ourColor.x, 0.0, 1.0),
+	FragColor = vec4(clamp(colorOut.x + ourColor.x, 0.0, 1.0),	
 	clamp(colorOut.y + ourColor.y, 0.0, 1.0),
 	clamp(colorOut.z + ourColor.z, 0.0, 1.0), 1.0);
+	FragColor = texture(ourTexture, texCoordOut);
 }
