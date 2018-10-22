6629c8723fe71366bb93981272a20a9b19201943
Yang Kyowon <kyowonhorang@gmail.com>
2018-10-22 15:02:55 +0900

    Mix with awesomeface.png

--- Resources/Shaders/BasicPS.glsl
+++ Resources/Shaders/BasicPS.glsl
@@ -4,12 +4,11 @@ uniform vec3 ourColor;
 in vec3 colorOut;
 in vec2 texCoordOut;
 
-uniform sampler2D ourTexture;
+uniform sampler2D texture1;
+uniform sampler2D texture2;
 
 void main()
 {
-	FragColor = vec4(clamp(colorOut.x + ourColor.x, 0.0, 1.0),	
-	clamp(colorOut.y + ourColor.y, 0.0, 1.0),
-	clamp(colorOut.z + ourColor.z, 0.0, 1.0), 1.0);
-	FragColor = texture(ourTexture, texCoordOut);
+	FragColor = mix(texture(texture1, texCoordOut), texture(texture2, texCoordOut), 0.2);
+	//FragColor = texture(_texture1, texCoordOut);
 }
