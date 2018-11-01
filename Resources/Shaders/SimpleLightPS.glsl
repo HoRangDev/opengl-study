8addec80f141b1bc177abaac4c00adb8757d227a
Yang Kyowon <kyowonhorang@gmail.com>
2018-11-01 09:43:25 +0900

    Phong Lighting Model

--- Resources/Shaders/SimpleLightPS.glsl
+++ Resources/Shaders/SimpleLightPS.glsl
@@ -3,12 +3,28 @@ out vec4 FragColor;
 
 uniform vec3 objectColor;
 uniform vec3 lightColor;
+uniform vec3 lightPos;
+uniform vec3 viewPos;
+
+in vec3 Normal;
+in vec3 FragPos;
 
 void main()
 {
-	float ambientStrength = 0.1f;
+	float ambientStrength = 0.1;
 	vec3 ambient = ambientStrength * lightColor;
 
-	vec3 result = ambient * objectColor;
+	vec3 norm = normalize(Normal);
+	vec3 lightDir = normalize(lightPos - FragPos); // Frag to light direction
+	float diff = max(dot(norm, lightDir), 0.0);
+	vec3 diffuse = diff * lightColor;
+
+	float specularIntensity = 0.5;
+	vec3 viewDir = normalize(viewPos - FragPos); // Frag to viewer
+	vec3 reflectDir = reflect( -lightDir, norm );
+	float spec = pow( max(dot(viewDir, reflectDir), 0.0 ), 32 );
+	vec3 specular = specularIntensity * spec * lightColor;
+
+	vec3 result = ( ambient + diffuse + specular ) * objectColor;
 	FragColor = vec4(result, 1.0);
 }
