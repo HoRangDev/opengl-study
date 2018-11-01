3e0b64ebb4c448c60bdfe4414dfc810c4c5a67fd
Yang Kyowon <kyowonhorang@gmail.com>
2018-11-01 10:47:14 +0900

    Using diffuse and specular maps

--- Resources/Shaders/SimpleLightPS.glsl
+++ Resources/Shaders/SimpleLightPS.glsl
@@ -1,9 +1,8 @@
 #version 330 core
 struct Material
 {
-	vec3 ambient;
-	vec3 diffuse;
-	vec3 specular;
+	sampler2D diffuse;
+	sampler2D specular;
 	float shininess;
 };
 
@@ -23,21 +22,22 @@ uniform Material material;
 uniform Light light;
 
 in vec3 Normal;
+in vec2 TexCoord;
 in vec3 FragPos;
 
 void main()
 {
-	vec3 ambient = light.ambient * material.ambient;
-
 	vec3 norm = normalize(Normal);
 	vec3 lightDir = normalize(light.position - FragPos); // Frag to light direction
 	float diff = max(dot(norm, lightDir), 0.0);
-	vec3 diffuse = diff * material.diffuse * light.diffuse;
 
 	vec3 viewDir = normalize(viewPos - FragPos); // Frag to viewer
 	vec3 reflectDir = reflect( -lightDir, norm );
 	float spec = pow( max(dot(viewDir, reflectDir), 0.0 ), material.shininess );
-	vec3 specular = spec * material.specular * light.specular;
+
+	vec3 ambient = light.ambient * vec3(texture(material.diffuse, TexCoord));
+	vec3 diffuse = diff * light.diffuse * vec3(texture(material.diffuse, TexCoord));
+	vec3 specular = spec * light.specular * vec3(texture(material.specular, TexCoord));
 
 	vec3 result = ( ambient + diffuse + specular );
 	FragColor = vec4(result, 1.0);
