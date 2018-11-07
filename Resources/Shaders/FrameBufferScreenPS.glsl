24f14627039bdb201c86903019f517d6c1d21ae0
Yang Kyowon <kyowonhorang@gmail.com>
2018-11-07 17:30:50 +0900

    Sharpen postprocess with kernel

--- Resources/Shaders/FrameBufferScreenPS.glsl
+++ Resources/Shaders/FrameBufferScreenPS.glsl
@@ -5,9 +5,39 @@ in vec2 TexCoords;
 
 uniform sampler2D screenTexture;
 
+const float offset = 1/300.0;
+
 void main()
 {
-	FragColor =  texture(screenTexture, TexCoords);
-	float avg = ( FragColor.r + FragColor.g + FragColor.b ) / 3.0;
-	FragColor = vec4(avg, avg, avg, 1.0f);
+	vec2 offsets[9] = vec2[](
+		vec2(-offset, offset),
+		vec2(0.0, offset),
+		vec2(offset, offset),
+		vec2(-offset, 0.0),
+		vec2( 0.0, 0.0 ),
+		vec2( offset, 0.0),
+		vec2( -offset, -offset),
+		vec2(0.0, -offset),
+		vec2(offset, -offset)
+	);
+
+	float kernel[9] = float[](
+	-1, -1, -1,
+	-1, 9, -1,
+	-1, -1, -1
+	);
+
+	vec3 sampleTex[9];
+	for (int idx = 0; idx < 9; ++idx)
+	{
+		sampleTex[idx] = vec3(texture(screenTexture, (TexCoords.st + offsets[idx])));
+	}
+
+	vec3 col = vec3(0.0);
+	for ( int idx = 0; idx < 9; ++idx )
+	{
+		col += (sampleTex[idx] * kernel[idx]);
+	}
+
+	FragColor =  vec4(col, 1.0);
 }
